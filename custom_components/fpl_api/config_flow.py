196a122cd93ab0b94e25125903ce4bde360199f1
Martin Højland <hojland93@gmail.com>
2021-10-10 22:14:49 +0200

    refactor: moving a lot and trying

--- custom_components/fpl_api/config_flow.py
+++ custom_components/fpl_api/config_flow.py
@@ -8,35 +8,75 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
+import homeassistant.helpers.config_validation as cv
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
-
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
+TEAMS = [
+    "Arsenal",
+    "Aston Villa",
+    "Brentford",
+    "Brighton",
+    "Burnley",
+    "Chelsea",
+    "Crystal Palace",
+    "Everton",
+    "Leeds",
+    "Leicester",
+    "Liverpool",
+    "Man City",
+    "Man Utd",
+    "Newcastle",
+    "Norwich",
+    "Southampton",
+    "Spurs",
+    "Watford",
+    "West Ham",
+    "Wolves",
+]
+
 
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
-        vol.Required("team_1"): str,
-        "team_2": str,
-        "team_3": str,
+        vol.Optional("fpl_email"): cv.string,
+        vol.Optional("fpl_password"): cv.string,
+        vol.Optional("fpl_user_id"): cv.positive_int,
+        vol.Optional("fav_team", default="Man Utd"): vol.In(TEAMS),
     }
 )
+DESCRIPTIONS = {
+    "fpl_email": "Email to log into Fantasy Premier League",
+    "fpl_password": "Password to log into Fantasy Premier League",
+    "fpl_user_id": "User id for your team. Find it on the site",
+    "fav_team": "Pick you favourite team to follow",
+}
 
 
 class PlaceholderHub:
-    """Placeholder class to make tests pass.
+    """Placeholder class to make tests pass."""
 
-    TODO Remove this placeholder class and replace with things from your PyPI package.
-    """
-
-    def __init__(self, tracked_teams: List[str]) -> None:
+    def __init__(
+        self, fpl_email: str, fpl_password: str, fpl_user_id: str, fav_team: str
+    ) -> None:
         """Initialize."""
-        self.tracked_teams = tracked_teams
+        self.fpl_email = fpl_email
+        self.fpl_password = fpl_password
+        self.fpl_user_id = fpl_user_id
+        self.fav_team = fav_team
 
     async def authenticate(self) -> bool:
         """Test if we can authenticate with the host."""
+        try:
+            fpl = FantasyPremierLeague(
+                self.fpl_email, self.fpl_password, self.fpl_user_id, self.fav_team
+            )
+            # await fpl.test_session()
+        except Exception:
+            return False
+
         return True
 
 
@@ -45,26 +85,18 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
     """
-    # TODO validate the data can be used to set up a connection.
+    fpl_email = data["fpl_email"] if "fpl_email" in data.keys() else None
+    fpl_password = data["fpl_password"] if "fpl_password" in data.keys() else None
+    fpl_user_id = data["fpl_user_id"] if "fpl_user_id" in data.keys() else None
+    fav_team = data["fav_team"] if "fav_team" in data.keys() else None
 
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
-    tracked_teams = [data["team_1"], data["team_2"], data["team_3"]]
-    hub = PlaceholderHub(tracked_teams)
+    hub = PlaceholderHub(fpl_email, fpl_password, fpl_user_id, fav_team)
 
     if not await hub.authenticate():
         raise InvalidAuth
 
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
     # Return info that you want to store in the config entry.
-    return {"title": f"FPL: {', '.join(tracked_teams)}"}
+    return {"title": "Fantasy Premier League Integration"}
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -78,7 +110,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         if user_input is None:
             return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+                step_id="user",
+                data_schema=STEP_USER_DATA_SCHEMA,
+                description_placeholders=DESCRIPTIONS,
             )
 
         errors = {}
@@ -96,7 +130,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title=info["title"], data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user",
+            data_schema=STEP_USER_DATA_SCHEMA,
+            errors=errors,
+            description_placeholders=DESCRIPTIONS,
         )
 
 
--- custom_components/fpl_api/sensor.py
+++ custom_components/fpl_api/sensor.py
@@ -1,59 +1,384 @@
 """Platform for sensor integration."""
 from __future__ import annotations
+import logging
+import aiohttp
+import jmespath
+from datetime import datetime, timedelta
+from typing import List
+from dateutil import parser as dateparser
+import pytz
+import asyncio
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.aiohttp_client import async_create_clientsession
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.event import track_point_in_time
 from .const import DOMAIN
+from .fpl_mod import FPL
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS: list[str] = ["sensor"]
+
+LIVE_SCAN_INTERVAL = timedelta(seconds=10)
+POSTGAME_SCAN_INTERVAL = timedelta(hours=1)
+DEFAULT_SCAN_INTERVAL = POSTGAME_SCAN_INTERVAL.seconds
 
 
-async def async_setup_entry(hass, config, async_add_entities):
-    """Set up the sensor platform."""
-
-    livescore = hass.data[DOMAIN][config.entry_id]
-
-    sensors = []
-    sensors.append(FPLSensor(livescore))
-    async_add_entities(sensors)
-
-
-def setup_platform(
+async def async_setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
-    add_entities: AddEntitiesCallback,
+    async_add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up the sensor platform."""
-    livescore = hass.data[DOMAIN][config.entry_id]
-    add_entities([FPLSensor(livescore)])
+
+    _LOGGER.info("Fantasy Premier League Sensor starting up")
+    session = async_create_clientsession(hass)
+
+    fpl_email = config.get("fpl_email")
+    fpl_password = config.get("fpl_password")
+    fpl_user_id = config.get("fpl_user_id")
+    fav_team = config.get("fav_team")
+
+    async_add_entities(
+        [FPLSensor(hass, session, fpl_email, fpl_password, fpl_user_id, fav_team)]
+    )
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Fantasy Premier League component."""
+    hass.data[DOMAIN] = {}
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up FPL Api from a config entry."""
+
+    session = async_create_clientsession(hass)
+
+    fpl_email = entry.data["fpl_email"] if "fpl_email" in entry.data else None
+    fpl_password = entry.data["fpl_password"] if "fpl_password" in entry.data else None
+    fpl_user_id = entry.data["fpl_user_id"] if "fpl_user_id" in entry.data else None
+    fav_team = entry.data["fav_team"] if "fav_team" in entry.data else None
+
+    hass.data[DOMAIN][entry.entry_id] = FPLSensor(
+        hass, session, fpl_email, fpl_password, fpl_user_id, fav_team
+    )
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
+
+
+def get_gameweek_score(player, gameweek):
+    gameweek_history = next(
+        history for history in player.history if history["round"] == gameweek
+    )
+    return gameweek_history["total_points"]
 
 
 class FPLSensor(SensorEntity):
-    """Representation of a Sensor."""
+    """
+    Primary exported interface for Soccer Livescore based on FPL wrapper.
+    """
 
     def __init__(
         self,
-        livescore,
+        hass=None,
+        session: aiohttp.ClientSession = None,
+        fpl_email: str = None,
+        fpl_password: str = None,
+        fpl_user_id: str = None,
+        fav_team: str = None,
+        tz="Europe/Copenhagen",
     ):
-        """Initialize the sensor."""
-        self._state = False
-        self.livescore = livescore
+        # self.entity_id = "sensor." + name.replace(" ", "_").lower()
+        self.hass = hass
+        self.session = session
+        self._state = "No games playing"
+        self._state_attributes = {}
+        self._scan_interval = DEFAULT_SCAN_INTERVAL
+        self.timer(datetime.today())
+
+        self.pytz_tz = pytz.timezone(tz)
+        self.fpl_email = fpl_email
+        self.fpl_password = fpl_password
+        self.fpl_user_id = fpl_user_id
+        self.fav_team = fav_team
+
+        self.day = 0
+        self.match_goals = []
+        self.id2team: dict = {}
+        self.team2id: dict = {}
+        self.active_gameweek: int = 0
+        self.kickoffs: List[datetime] = []
+        self.fav_team_id: str = ""
 
     @property
-    def name(self) -> str:
-        """Return the name of the sensor."""
-        return "FPL API"
+    def should_poll(self):
+        """Polling required."""
+        return True
+
+    @property
+    def icon(self):
+        """Return the icon to use in the frontend."""
+        return "mdi:soccer-field"
 
     @property
     def state(self):
         """Return the state of the sensor."""
         return self._state
 
-    async def update(self):
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes of the sensor."""
+        return self._state_attributes
+
+    def timer(self, nowtime):
+        self.schedule_update_ha_state(True)
+        polling_delta = self.set_polling()
+        nexttime = nowtime + polling_delta
+        # Setup timer to run again at polling delta
+        track_point_in_time(self.hass, self.timer, nexttime)
+
+    async def test_session(self):
+        async with aiohttp.ClientSession() as session:
+            fpl = FPL(session)
+            await fpl.async_init(self.hass)
+            if self.fpl_email and self.fpl_password:
+                fpl = FPL(session)
+                await fpl.async_init(self.hass)
+                await fpl.login(email=self.fpl_email, password=self.fpl_password)
+                if self.fpl_user_id:
+                    self.user = await fpl.get_user(self.fpl_user_id)
+            else:
+                fpl = FPL(session)
+                await fpl.async_init(self.hass)
+
+    async def scroll_day(self):
+        self.id2team = await self.get_id2team()
+        self.team2id = {team: id for id, team in self.id2team.items()}
+        self.active_gameweek = await self.get_active_gameweek()
+        self.fav_team_id = self.team2id[self.fav_team]
+        self.kickoffs = await self.get_fixture_kickoffs()
+        self.match_goals = []
+
+    async def get_team(self):
+        async with aiohttp.ClientSession() as session:
+            fpl = FPL(session)
+            await fpl.async_init(self.hass)
+            # fpl.init()
+            if self.fpl_email and self.fpl_password:
+                await fpl.login(email=self.fpl_email, password=self.fpl_password)
+                self.user = await fpl.get_user(self.fpl_user_id)
+                team = await self.user.get_team()
+                player_ids = [player["element"] for player in team]
+                # player_summaries = await fpl.get_player_summaries(
+                #     player_ids, return_json=True
+                # )
+                players = await fpl.get_players(player_ids, include_summary=True)
+                player_score = {
+                    f"{player.first_name} {player.web_name}": get_gameweek_score(
+                        player, self.active_gameweek
+                    )
+                    for player in players
+                }
+                top_scorer = max(
+                    players, key=lambda x: get_gameweek_score(x, self.active_gameweek)
+                )
+
+                return team, top_scorer
+            else:
+                return "not authenticated"
+
+    async def get_id2team(self):
+        async with aiohttp.ClientSession() as session:
+            fpl = FPL(session)
+            await fpl.async_init(self.hass)
+            id2teams = {}
+            for i in range(1, 21, 1):
+                res = await fpl.get_team(i, return_json=True)
+                id2teams[i] = res["name"]
+        return id2teams
+
+    async def get_pl_teams(self):
+        async with aiohttp.ClientSession() as session:
+            fpl = FPL(session)
+            await fpl.async_init(self.hass)
+            id2teams = {}
+            for i in range(1, 21, 1):
+                res = await fpl.get_team(i, return_json=True)
+                id2teams[i] = res["name"]
+        return sorted(list(id2teams.values()))
+
+    async def get_active_gameweek(self):
+        async with aiohttp.ClientSession() as session:
+            fpl = FPL(session)
+            await fpl.async_init(self.hass)
+            # fpl.init()
+            gameweeks = await fpl.get_gameweeks(return_json=True)
+        active_gameweek = jmespath.search("[?is_current].id | [0]", gameweeks)
+        return active_gameweek
+
+    async def get_fixture_kickoffs(self):
+        async with aiohttp.ClientSession() as session:
+            fpl = FPL(session)
+            await fpl.async_init(self.hass)
+            # fpl.init()
+            fixtures = await fpl.get_fixtures_by_gameweek(
+                gameweek=self.active_gameweek, return_json=True
+            )
+            fixtures = jmespath.search(
+                "[?finished==`false` && started==`false`].{team_a: team_a, team_h: team_h, kickoff_time: kickoff_time}",
+                fixtures,
+            )
+            fixtures = [
+                dateparser.parse(fixture["kickoff_time"])
+                .replace(tzinfo=pytz.utc)
+                .astimezone(tz=self.pytz_tz)
+                if fixture["team_a"] in self.fav_team_id
+                or fixture["team_h"] in self.fav_team_id
+                else None
+                for fixture in fixtures
+            ]
+            fixtures = [x for x in fixtures if x is not None]
+        return fixtures
+
+    async def get_live_fixtures(self):
+        async with aiohttp.ClientSession() as session:
+            fpl = FPL(session)
+            await fpl.async_init(self.hass)
+            # fpl.init()
+            fixtures = await fpl.get_fixtures_by_gameweek(
+                gameweek=self.active_gameweek, return_json=True
+            )
+            fixtures = jmespath.search(
+                "[?finished==`false` && started==`true`].{team_a: team_a, team_h: team_h, stats: stats, id: id}",
+                fixtures,
+            )
+            fav_team_fixtures = [
+                fixture
+                if fixture["team_a"] in self.fav_team_id
+                or fixture["team_h"] in self.fav_team_id
+                else None
+                for fixture in fixtures
+            ]
+            fav_team_fixtures = [x for x in fav_team_fixtures if x is not None]
+
+            goals_scored = jmespath.search(
+                "[].stats[?contains(identifier, 'goal') == `true`].{a: a, h: h}",
+                fav_team_fixtures,
+            )
+
+            teams_per_match = [
+                f"{self.id2team[fixture['team_h']]} v. {self.id2team[fixture['team_a']]}"
+                for fixture in fav_team_fixtures
+            ]
+            goals_scored_per_match = [
+                {
+                    "home_goals": len(fixture[0]["h"]) + len(fixture[1]["h"]),
+                    "away_goals": len(fixture[0]["a"]) + len(fixture[1]["a"]),
+                }
+                for fixture in goals_scored
+            ]  # both goals and own goals
+
+            # goal_scorers_per_match = [{fixture[0][""]} for fixture in goals_scored]
+            # todo doesn't seem to include overtime goals
+            match_goals = dict(zip(teams_per_match, goals_scored_per_match))
+            return match_goals
+
+    async def get_match_goals(self):
+        match_goals = await self.get_live_fixtures()
+        if not self.match_goals:
+            self.match_goals = match_goals
+        new_goals = [
+            match_goals[match] == score
+            for match, score in self.match_goals.items()
+            if match in match_goals.keys()
+        ]
+        new_goal = any(new_goals)  # TODO WHO SCORED
+        self.match_goals = match_goals
+        return new_goal, match_goals
+
+    async def async_update(self):
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        new_goal = await self.livescore.update()
+        _LOGGER.debug("Fetching data from FPL")
+        now = datetime.today().astimezone(tz=self.pytz_tz)
+        if now.day != self.day:
+            self.day = now.day
+            await self.scroll_day()
 
-        self._state = new_goal
+        new_goal, match_goals = await self.get_match_goals()
+        team, top_scorer = await self.get_team()
+        new_goal = {"new_goal": new_goal}
+        top_scorer = {
+            "top_scorer": f"{top_scorer.first_name} {top_scorer.web_name}: {get_gameweek_score(top_scorer, self.active_gameweek)}"
+        }
+
+        # Move tracked team to here later
+        ## Set attribute for goal scored by tracked team.
+        # if self._state_attributes.get("goal_team_id", None) == self._team_id:
+        #    self._state_attributes["goal_tracked_team"] = True
+        # else:
+        #    self._state_attributes["goal_tracked_team"] = False
+
+        all_attr = {**new_goal, **top_scorer}  # **match_goals,
+        self._state_attributes = all_attr
+        self._state = (
+            "In Progress"
+            if any(
+                [
+                    kickoff < now < kickoff + timedelta(hours=2)
+                    for kickoff in self.kickoffs
+                ]
+            )
+            else "No games playing"
+        )
+
+        _LOGGER.debug("Done fetching data from FPL")
+
+    @property
+    def name(self) -> str:
+        """Return the name of the sensor."""
+        return "Fantasy Premier League Sensor"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def set_polling(self):
+        game_state = self._state
+        if game_state in [None, "In Progress"]:
+            if self._scan_interval > LIVE_SCAN_INTERVAL:
+                polling_delta = self._scan_interval
+            else:
+                polling_delta = LIVE_SCAN_INTERVAL
+        else:
+            polling_delta = POSTGAME_SCAN_INTERVAL
+        return polling_delta
